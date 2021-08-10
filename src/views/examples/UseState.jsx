fdead5c722d60273d2144778641c06ec7dd1dc37
Maro de Melo <marodemelo10@gmail.com>
2021-08-10 19:26:12 -0300

    utilizando o setState

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+
+import SectoinTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(1);
 
-export default UseState
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectoinTitle title="Exercicio 01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
+          <button className="btn" onClick={() => setCount(1000)}>
+            1000
+          </button>
+          <button
+            className="btn"
+            onClick={() => setCount((current) => current + 1000)}
+          >
+            +1000
+          </button>
+        </div>
+      </div>
+
+      <SectoinTitle title="Exercicio 02" />
+    </div>
+  );
+};
+
+export default UseState;
