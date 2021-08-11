88dfab12462aef9a6d1e5399540a34721600e058
Maro de Melo <marodemelo10@gmail.com>
2021-08-10 21:18:04 -0300

    usando useEffect para passar efeitos colaterais entre os parametros do componente

---
+++ lhoco.js
@@ -0,0 +1,13 @@
+console.log(fatorial(4))
+
+function fatorial (number){
+    let fat = 1;
+    
+    while (number>1) {
+      fat = (fat) * (number-1);
+      
+    }
+    return fat;
+  };
+
+ 
--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectoinTitle from "../../components/layout/SectionTitle";
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectoinTitle title="Exercicio 01" />
+      <div className="center">
+        <div>
+          <span className="text ">Fatorial: </span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          onChange={(e) => setNumber(e.target.value)}
+          value={number}
+        />
+      </div>
+      <SectoinTitle title="Exercicio 02" />
+    </div>
+  );
+};
+
+export default UseEffect;
